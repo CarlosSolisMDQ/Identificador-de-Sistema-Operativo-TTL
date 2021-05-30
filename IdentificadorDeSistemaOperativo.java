cac11445bb6af838f772c204f75483a18ccecdf6
Carlos Solis <46057168+CarlosSolisMDQ@users.noreply.github.com>
2021-05-30 20:42:29 -0300

    Add files via upload

---
+++ IdentificadorDeSistemaOperativo.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class IdentificadorDeSistemaOperativo {
+
+
+    public static void main(String[] args) throws IOException {
+        Process process = Runtime.getRuntime().exec("ping -c 1 "+ args[0]);
+        InputStream input = new BufferedInputStream(process.getInputStream());
+        InputStreamReader inputStream = new InputStreamReader(input);
+        BufferedReader bufferReader = new BufferedReader(inputStream);
+
+
+        String output = null;
+        while((bufferReader.readLine())!=null){
+            output += bufferReader.readLine();
+        }
+
+        String[] array = output.split(" ");
+        Pattern pattern = Pattern.compile("^ttl.*");
+        String stringTTL = null;
+
+        for(String word: array){
+            Matcher match = pattern.matcher(word);
+            if (match.matches()) {
+                stringTTL = word;
+            }
+        }
+        stringTTL = stringTTL.replaceAll("\\D+", "");
+
+        int ttl = Integer.parseInt(stringTTL);
+
+        if (ttl >= 0 && ttl <= 64){
+            System.out.println("La maquina objetivo es un linux");
+        } else if (ttl >= 65 && ttl <= 128){
+            System.out.println("La maquina objetivo es un windows");
+        }
+
+    }
+}
